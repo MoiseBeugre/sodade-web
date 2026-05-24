36f9515fad2d02fe4ba34721d9f26c82871c407a
Moise Beugre <moise.beugre@icloud.com>
2026-05-24 18:19:51 -0400

    redirect home to auth login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-'use client';
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <h1 className="text-2xl text-green-600 font-bold">Sodade</h1>
-    </div>
-  );
+  redirect('/auth/login');
 }
